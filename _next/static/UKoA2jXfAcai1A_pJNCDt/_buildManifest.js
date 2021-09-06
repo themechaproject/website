a3813739613f0ee94a070d380eebef5a8aeb4bd2
MayorLamp <90140429+MayorLamp@users.noreply.github.com>
2021-09-06 08:37:55 +0000

    Deploying to gh-pages from @ themechaproject/website@3d9cb46ab1fa69cc2ce36a85425db8eef488b075 🚀

--- _next/static/UKoA2jXfAcai1A_pJNCDt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed572b11a2318e005aaa.css","static/chunks/pages/index-5ea9bb01bdefdedc1930.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
