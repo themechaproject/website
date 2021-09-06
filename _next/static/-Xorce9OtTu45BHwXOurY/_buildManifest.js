c5de857f2e5fac5f65199c3d65203c63314c41e3
MayorLamp <90140429+MayorLamp@users.noreply.github.com>
2021-09-06 18:36:10 +0000

    Deploying to gh-pages from @ themechaproject/website@42b4d6a3d12830b3b54a416d3cb2b551e8fb012f 🚀

--- _next/static/-Xorce9OtTu45BHwXOurY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed572b11a2318e005aaa.css","static/chunks/pages/index-a7e540261093489bc841.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/xsftlbhNh7nRTt5bORmXp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed572b11a2318e005aaa.css","static/chunks/pages/index-264b93ac4def824cb8aa.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
